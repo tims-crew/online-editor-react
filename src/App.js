9d73c06dd4c60894fc4e6c1c0fe6852f92c78357
tims-crew <timverstalshik@gmail.com>
2022-03-16 23:33:10 +0500

    deploy: heroku

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Editor from './components/Editor';
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
+  const [html, setHtml] = useLocalStorage('html', '');
+  const [css, setCss] = useLocalStorage('css', '');
+  const [js, setJs] =  useLocalStorage('javascript', '');
   const [srcDoc, setSrcDoc] = useState()
-  const [html, setHtml] = useState();
-  const [css, setCss] = useState();
-  const [js, setJs] = useState();
-
 
   useEffect(() => {
-    setSrcDoc(`
-      <body>${html}</body>
-      <style>${css}</style>
-      <script>${js}</script>
-    `
-    )
-  })
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+        `
+      )
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
 
   return (
     <div className="App">
       <div className="pane top-pane">
-        <Editor />
-        <Editor />
-        <Editor />
+        <Editor 
+          // label="HTML"
+          language="xml"
+          label="html"
+          value={html}
+          onChange={setHtml}
+        />
+
+        <Editor 
+          language="css"
+          label="css"
+          value={css}
+          onChange={setCss}
+        />
+
+        <Editor 
+          language="javascript"
+          label="js"
+          value={js}
+          onChange={setJs}
+        />
+        {/* <Editor />
+        <Editor /> */}
       </div>
       <div className="bottom-pane">
         <iframe
           srcDoc={srcDoc}
           frameBorder={'0'}
           width={'100%'}
-          height={'50%'}
+          height={'100%'}
         />
       </div>
     </div>
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,70 @@
-import React from 'react';
+import React, { useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/css/css";
+import "codemirror/mode/javascript/javascript";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExpandAlt, faCompressAlt } from '@fortawesome/free-solid-svg-icons';
+import { EditorView } from "@codemirror/view";
 
 export default function Editor(props) {
-    const [value, onChange] = props;
+  const [open, toggleOpen] = useState(true);
+  const { language, label, value, onChange } = props;
+
+  let myTheme = EditorView.theme(
+    {
+      "&": {
+        color: "white",
+        backgroundColor: "#034",
+      },
+      ".cm-content": {
+        caretColor: "#0e9",
+      },
+      "&.cm-focused .cm-cursor": {
+        borderLeftColor: "#0e9",
+      },
+      "&.cm-focused .cm-selectionBackground, ::selection": {
+        backgroundColor: "#074",
+      },
+      ".cm-gutters": {
+        backgroundColor: "#045",
+        color: "#ddd",
+        border: "none",
+      },
+    },
+    { dark: true }
+  );
+
+  function handleChange() {
+    onChange();
+  }
+
+  return (
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
+      <div className="editor-title">
+        {label}
+        <button className="expand-collapse-btn">
+          <FontAwesomeIcon icon={open ? faExpandAlt : faCompressAlt}/>
+        </button>
+      </div>
+
+
+      <CodeMirror
+        value={value}
+        className="code-mirror-wrapper"
+        width="29rem"
+        height="100%"
+        language={language}
+        onChange={onChange}
+        theme={myTheme}
+        options={{
+          mode: "xml",
+          lineWrapping: true,
+          lint: true,
+        }}
+      />
+    </div>
+  );
 }
